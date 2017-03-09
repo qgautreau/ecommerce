d647e73a96bbfb651c0abbcf20ccdeccbe14dc17
bl00m <leon.pierre@openmailbox.org>
2017-03-09 14:52:49 +0100

    Split script files

---
+++ static/js/script_catalogue.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    console.log('start');
+    displayProducts(catalog, 0);
+    setupPagination();
+
+    $('.pagination li > a').click(function(event) { // TODO
+        var needUpdate = false;
+        if ($(this).attr('aria-label') == 'Previous' && curntPagination > 0) {
+            curntPagination -= 10;
+            needUpdate = true;
+
+        } else if ($(this).attr('aria-label') == 'Next' && curntPagination+10 <= maxPagination) {
+            curntPagination += 10;
+            needUpdate = true;
+        }
+        if(needUpdate){
+            setupPagination();
+            displayProducts(catalog, curntPagination);
+        }
+        event.preventDefault();
+    });
+});
---
+++ static/js/script_home.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    console.log('start');
+    displayProducts(getRandomProducts(catalog), 0);
+});
