6d0dd91ebb5fe91cc745249d9b8f1a6add008d2b
Quentin Gautreau <qgautreau@gmail.com>
2017-03-09 15:02:12 +0100

    move fonction get

--- static/js/script.js
+++ static/js/script.js
@@ -107,3 +107,7 @@ function setupPagination() {
         lastPaginationItem.before(listElement);
     }
 }
+
+var GET_PARAM = function(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+};
--- static/js/script_produit.js
+++ static/js/script_produit.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-    var GET_PARAM = function(name) {
-        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-    };
     console.log(GET_PARAM('name'));
 });
