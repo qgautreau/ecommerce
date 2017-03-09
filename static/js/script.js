47e706d5f46b51b94378fa79967566849fb4ed7d
Quentin Gautreau <qgautreau@gmail.com>
2017-03-09 14:46:18 +0100

    start script produit

--- static/js/script.js
+++ static/js/script.js
@@ -140,5 +140,12 @@ $(document).ready(function() {
                 event.preventDefault();
             });
             break;
+
+        case 'Produit':
+            var GET_PARAM = function(name) {
+                return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+            };
+            console.log(GET_PARAM('name'));
+            break;
     }
 });
