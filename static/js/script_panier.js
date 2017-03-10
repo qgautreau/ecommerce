0d72c3f568a5ee9fe8b340c937b9839af6150049
bl00m <leon.pierre@openmailbox.org>
2017-03-10 11:51:20 +0100

    Add clear option on panier page

--- static/js/script_panier.js
+++ static/js/script_panier.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     var panier = localStorage.getItem('panier');
     panier = JSON.parse(panier);
     genPanier(panier, $('#panier > tbody'));
+
+    $('#clear_panier').click(function() {
+        localStorage.clear();
+        $('#panier > tbody').empty();
+    });
 });
